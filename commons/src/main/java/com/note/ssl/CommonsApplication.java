303e5fae3e199cee2115f84b454bd21bd27bc492
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-10-10 21:20:54 +0800

    commons

---
+++ commons/src/main/java/com/note/ssl/CommonsApplication.java
@@ -0,0 +1,9 @@
+package com.note.ssl;
+
+
+
+public class CommonsApplication {
+//    public static void main(String[] args) {
+//        SpringApplication.run(CommonsApplication.class, args);
+//    }
+}
---
+++ commons/src/main/java/com/note/ssl/commons/constant/ApiConstant.java
@@ -0,0 +1,21 @@
+package com.note.ssl.commons.constant;
+
+/**
+ * @Author: SongShengLin
+ * @Date: 2022/10/10 21:17
+ * @Describe:
+ */
+public class ApiConstant {
+    // 成功
+    public static final int SUCCESS_CODE = 1;
+    // 成功提示信息
+    public static final String SUCCESS_MESSAGE = "Successful.";
+    // 错误
+    public static final int ERROR_CODE = 0;
+    // 未登录
+    public static final int NO_LOGIN_CODE = -100;
+    // 请登录提示信息
+    public static final String NO_LOGIN_MESSAGE = "Please login.";
+    // 错误提示信息
+    public static final String ERROR_MESSAGE = "Oops! Something was wrong.";
+}
